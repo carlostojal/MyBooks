704f139e5bf03cb91327fecf250c9e2da8763d14
Carlos Tojal <carlos.tojal@hotmail.com>
2019-06-25 19:42:37 +0100

    Added book write to csv file.

--- app/src/main/java/com/carlostojal/mybooks/AddBook.java
+++ app/src/main/java/com/carlostojal/mybooks/AddBook.java
@@ -7,10 +7,12 @@ package com.carlostojal.mybooks;
 // github.com/carlostojal/MyBooks
 //
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,37 +20,83 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+
 public class AddBook extends Fragment {
     public static final String TAG = "AddBookFragment";
 
     //Interface elements declaration
-    private TextView titleText;
-    private TextView writerText;
-    private TextView publisherText;
-    private TextView yearText;
-    private TextView npagesText;
     private EditText titleField;
     private EditText writerField;
     private EditText publisherField;
     private EditText yearField;
     private EditText npagesField;
+    private Button addbook_button;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_book,container,false);
 
-        titleText = (TextView) view.findViewById(R.id.title);
-        writerText = (TextView) view.findViewById(R.id.writer);
-        publisherText = (TextView) view.findViewById(R.id.publisher);
-        yearText = (TextView) view.findViewById(R.id.year);
-        npagesText = (TextView) view.findViewById(R.id.npages);
         titleField = (EditText) view.findViewById(R.id.title_field);
         writerField = (EditText) view.findViewById(R.id.writer_field);
         publisherField = (EditText) view.findViewById(R.id.publisher_field);
         yearField = (EditText) view.findViewById(R.id.year_field);
         npagesField = (EditText) view.findViewById(R.id.npages_field);
+        addbook_button = (Button) view.findViewById(R.id.addbook_button);
+
+        addbook_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddBook(getContext());
+            }
+        });
 
         return view;
+    }
+
+    public void onAddBook(Context context) {
+        Toast.makeText(getActivity(),"onAddBook()",Toast.LENGTH_SHORT).show();
+        String title = titleField.getText().toString(); //obligatory
+        String writer = writerField.getText().toString() ; //obligatory
+        String publisher = publisherField.getText().toString();
+        int year = Integer.parseInt(yearField.getText().toString());
+        int npages = Integer.parseInt(npagesField.getText().toString()); //obligatory
+
+        if(!title.equals("")&&!writer.equals("")&&npages>0) {
+            try {
+                Toast.makeText(getActivity(),"Please wait...",Toast.LENGTH_SHORT).show();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("books.csv",Context.MODE_APPEND));
+                outputStreamWriter.write(title);
+                outputStreamWriter.write(";");
+                outputStreamWriter.write(writer);
+                outputStreamWriter.write(";");
+                outputStreamWriter.write(publisher);
+                outputStreamWriter.write(";");
+                outputStreamWriter.write(String.valueOf(year));
+                outputStreamWriter.write(";");
+                outputStreamWriter.write(String.valueOf(npages));
+                outputStreamWriter.write("\n");
+                outputStreamWriter.close();
+                Toast.makeText(getActivity(),"Book added successfully.",Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getActivity(),"Error.",Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getActivity(),"Error.",Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+            Toast.makeText(getActivity(),"Obligatory fields can't be left empty.",Toast.LENGTH_SHORT).show();
     }
 }
