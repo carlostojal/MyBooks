f87567dfb7e48c76bd0b6798101660671eedd1df
Carlos Tojal <carlos.tojal@hotmail.com>
2019-06-25 16:04:25 +0100

    Add book fragment layout design

--- app/src/main/java/com/carlostojal/mybooks/MainActivity.java
+++ app/src/main/java/com/carlostojal/mybooks/MainActivity.java
@@ -16,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import com.carlostojal.mybooks.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +23,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
     }
 }
