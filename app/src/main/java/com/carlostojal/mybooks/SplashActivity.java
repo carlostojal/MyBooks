12ec4601d55b91f44a18a816eaa09869cf87d674
Carlos Tojal <carlos.tojal@hotmail.com>
2019-06-28 13:39:22 +0100

    Added scrollviews.

---
+++ app/src/main/java/com/carlostojal/mybooks/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.carlostojal.mybooks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showMainActivity();
+            }
+        },2000);
+    }
+
+    protected void showMainActivity() {
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
